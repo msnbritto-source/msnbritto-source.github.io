a03d24a1a099e828b40af3e76c5fde4b176d2930
msnbritto-source <msnbritto@gmail.com>
2025-09-14 00:41:33 +0530

    Update PortfolioSite.jsx

--- src/PortfolioSite.jsx
+++ src/PortfolioSite.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+
+function PortfolioSite() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
+      <p className="text-lg text-gray-600">
+        This is where I’ll showcase my work using React + TailwindCSS 🚀
+      </p>
+    </div>
+  );
+}
+
+export default PortfolioSite;
 

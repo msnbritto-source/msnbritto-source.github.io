6e5f3cd698267211da0568fb70b899e6bfa84772
msnbritto-source <msnbritto@gmail.com>
2025-09-14 23:15:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",  // since your repo is msnbritto-source.github.io
-});
-
+  base: '/',   // 👈 for user.github.io
+})
